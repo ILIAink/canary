474d4a2ca7003036d5eebc9bf93d0390d79e629f
Tao Groves <>
2024-02-23 17:51:48 -0500

    Login backend complete - Added Static Bootstrap and overrode some allauth templates

--- canary/views.py
+++ canary/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# home view - info about the app and a link to the login page
+def home(request):
+    return render(request, 'home.html')
+
+# login view - a simple login page
+def login(request):
+    return render(request, 'login.html')
+
+# dashboard view - a simple dashboard page
+# TODO implement this
+def dashboard(request):
+    return render(request, 'dashboard.html')
