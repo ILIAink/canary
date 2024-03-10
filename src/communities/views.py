73366ee721ec165b9febded35ca31fad4ef90b8d
JCDster <wzg6qh@virginia.edu>
2024-03-10 13:36:16 -0400

    Working on making community running from the old code

--- src/communities/views.py
+++ src/communities/views.py
@@ -13,7 +13,7 @@ def create_community(request):
 
 # Admin home view - displays the home view for the admin of a community
 def admin_community_home(request):
-    return render(request, 'communityadmin/admin_community_home.html')
+    return render(request, 'dashboard/dashboard_community_admin.html')
 
 
 # def for saving a community after creation
@@ -21,16 +21,17 @@ def save_community(request):
 
     name = request.POST.get('name')
     password = request.POST.get('password')
+    description = request.POST.get('description')
     admin = request.user
 
-    community = Community(name=name, password=password)
+    community = Community(name=name, password=password, description=description)
 
     # Save the report to the database
     community.save()
 
-    adminmember = CommunityMember(community=community, admin=1, member=admin)
+    admin_member = CommunityMember(community=community, admin=1, member=admin)
 
-    adminmember.save()
+    admin_member.save()
 
     return HttpResponseRedirect(reverse("admin_community_home", args=community.name))
 
