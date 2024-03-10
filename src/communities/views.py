86b23fcad2691bff40cd7f5d8a35aa19025ca8fe
JCDster <wzg6qh@virginia.edu>
2024-03-10 13:22:00 -0400

    Working on view

--- src/communities/views.py
+++ src/communities/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def create_community(request):
+    return render(request, 'create_community.html')
+
+
+# Admin home view - displays the home view for the admin of a community
+def admin_community_home(request):
+    return render(request, 'communityadmin/admin_community_home.html')
+
+
+# def for saving a community after creation
+def save_community(request):
+
+    name = request.POST.get('name')
+    password = request.POST.get('password')
+    admin = request.user
+
+    community = Community(community_name=name, password=password, admin=admin)
+
+    # Save the report to the database
+    community.save()
