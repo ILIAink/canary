95596d14435967cab21f4049f342abce9551ff5f
JCDster <wzg6qh@virginia.edu>
2024-03-13 14:19:37 -0400

    Push, made join community method but it has problems

--- src/communities/views.py
+++ src/communities/views.py
@@ -36,7 +36,7 @@ def save_community(request):
 
 
 
-    # Save the report to the database
+    # Save the community to the database
     community.save()
 
     admin_member = CommunityMember(community=community, is_admin=1, member=request.user)
@@ -46,5 +46,31 @@ def save_community(request):
     return HttpResponseRedirect("admin_community_home")
 
 def join_community(request):
-    #see if community is valid, if is, see if password is valid, if is make relation, if not error page
-    return HttpResponseRedirect("join_success")
+    if request.method == 'POST':
+        community_name = request.POST.get('name', None)
+        password = request.POST.get('password', None)
+
+        if community_name is not None and password is not None:
+            try:
+                # Search for the community by name
+                community = Community.objects.get(name=community_name)
+
+                # Check if the provided password matches the community password
+                if community.password == password:
+                    # Password matches, perform further actions here
+                    # For example, you might redirect to a page displaying community details
+                    member = CommunityMember(community=community, is_admin=0, member=request.user)
+                    member.save()
+                    return HttpResponseRedirect("join_success")
+                else:
+                    # Password does not match
+                    return HttpResponseRedirect("join_community_error")
+            except Community.DoesNotExist:
+                # Community with given name does not exist
+                return HttpResponseRedirect("join_community_error")
+        else:
+            # Invalid POST data
+            return HttpResponseRedirect("join_community_error")
+    else:
+        # GET request, render a form to search for the community
+        return HttpResponseRedirect("join_community_error")
