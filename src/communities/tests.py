6ad29bebf22bdd37facf7596d63ead67bc568257
liongoogle <113564377+liongoogle@users.noreply.github.com>
2024-03-16 13:38:03 -0400

    commit with broken tests, before deleting __init__.py

--- src/communities/tests.py
+++ src/communities/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.apps import AppConfig
+from src.communities.models import Community
 
-# Create your tests here.
+
+class CommunityTests(TestCase):
+    def test_communities(self):
+
+        test_community = Community.objects.create(name='test_community', password='123',description='test_description')
+
+        response = self.client.get(reverse("communities:dashboard", args=(test_community.id)))
+        print(response)
+        assert response.status_code == 200
