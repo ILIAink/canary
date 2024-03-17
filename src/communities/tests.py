d7373cbdefece254c75ef96291a7cb8a3029b95e
liongoogle <113564377+liongoogle@users.noreply.github.com>
2024-03-16 21:02:54 -0400

    3 robust tests completed

--- src/communities/tests.py
+++ src/communities/tests.py
@@ -20,7 +20,7 @@ class CommunityTests(TestCase):
     testUser = userModel.objects.create_user(email="test_user@gmail.com", username='testuser', password="123")
     request.user = testUser
 
-    # call function we are testing
+    # view being tested
     save_community(request)
 
     community = Community.objects.get(name="testing quality enforcement")
@@ -32,4 +32,48 @@ class CommunityTests(TestCase):
     self.assertIsNotNone(admin)
 
 
+  def test_join_community_valid_community(self):
+    userModel = apps.get_model('accounts', 'User')
+    testUser = userModel.objects.create_user(email="test_user@gmail.com", username='testuser', password="123")
+    testUser.save()
 
+    request = HttpRequest()
+    request.method = "POST"
+    request.POST = {'name': 'test_community', 'password': "123"}
+    request.user = testUser
+
+    test_community = Community(name="test_community", description="report bad code", password="123")
+    test_community.save()
+
+    # view being tested
+    join_community(request)
+
+    member = CommunityMember.objects.get(community=test_community, member=testUser, is_admin=False)
+    self.assertIsNotNone(member)
+    self.assertEqual(member.member.username, testUser.username)
+    self.assertEqual(member.member.password, testUser.password)
+
+
+  def test_save_report_authenticated_user(self):
+    userModel = apps.get_model('accounts', 'User')
+    testUser = userModel.objects.create_user(email="test_user@gmail.com", username='testuser', password="123")
+    testUser.save()
+
+    request = HttpRequest()
+    request.method = 'POST'
+    request.POST = {'title': 'test_report', 'content': 'this is a report', 'resolution_method': 'Canary'}
+    request.user = testUser
+
+    test_community = Community(name="test_community", description="report bad code", password="123")
+    test_community.save()
+
+    # view being tested
+    save_report(request, test_community.id)
+
+    # check for existence of report
+    report = Report.objects.get(title='test_report', content='this is a report', author=testUser)
+    self.assertIsNotNone(report)
+    self.assertEqual(report.author, testUser)
+    self.assertEqual(report.content, 'this is a report')
+    # check that community acknowledges report
+    self.assertIn(report, test_community.reports.all())
