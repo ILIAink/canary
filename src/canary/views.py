25c1246d32b215fe42053a7eca10bdd53f5b8e87
liongoogle <113564377+liongoogle@users.noreply.github.com>
2024-04-04 19:21:10 -0400

    updated user dashboard to show separete admin and user communities

--- src/canary/views.py
+++ src/canary/views.py
@@ -18,11 +18,12 @@ def login(request):
 # dashboard view - displays the user's dashboard
 def dashboard(request):
    if request.user.is_authenticated:
-       user_communities = CommunityMember.objects.filter(member=request.user)
+       user_communities = CommunityMember.objects.filter(member=request.user, is_admin=False)
+       admin_communities = CommunityMember.objects.filter(member=request.user, is_admin=True)
        if False:#request.user.is_staff:  # Check if the user is an administrator
            return render(request, 'dashboard/dashboard_admin.html', {'user': request.user, 'user_communities': user_communities})
        else:
-           return render(request, 'dashboard/dashboard_user.html', {'user': request.user, 'user_communities': user_communities})
+           return render(request, 'dashboard/new_user_dashboard.html', {'user': request.user, 'user_communities': user_communities, "admin_communities": admin_communities})
    else:
        # Handle the case when the user is not authenticated, perhaps redirect to login page
        return render(request, 'account/login.html')
