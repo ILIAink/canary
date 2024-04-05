49c9deb58047359d28ed1e5f6f9657c7a06795bb
Nathan Luu <nluu0122@gmail.com>
2024-04-04 23:10:38 -0400

    users can now edit profile (username, first name, last name)

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from accounts.models import User
+from django.contrib import messages
+
+
 
 # Create your views here.
+
+def edit_profile(request):
+    return render(request, 'account/edit_profile.html')
+# save user info edits
+def save_profile(request):
+    user = User.objects.get(id=request.user.id)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+
+        user.save()
+        messages.success(request, "User Info Successfully Updated!")
+        return HttpResponseRedirect(reverse("canary:dashboard"))
+
+
+
+
+
