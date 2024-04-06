63eadc2060fc6949029bc575a67640c64e3454fe
ILIAink~ <ysk.iliakhalighi@gmail.com>
2024-04-06 10:28:19 -0400

    Allauth templates overriden, new styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/tw-elements/js/**/*.js"],
   theme: {
     extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateY(-50%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 0.3s ease-out',
+      },
       colors: {
-        "main-bg": "#1a1a1a",
+        "main-bg": "#1E1C1C",
         "button-bg": "rgba(239, 241, 246, 0.75)",
         "button-text": "rgba(255, 255, 255, 0.1)",
         "text-color": "#ffffff",
@@ -19,9 +28,9 @@ module.exports = {
         },
         lc: {
           900: "#262626",
-          800: "#282828",
+          800: "#323232",
           700: "#2a2a2a",
-          500: "#4a4a4a",
+          500: "#252525",
           300: "#595959",
           200: "#5c5c5c",
         },
