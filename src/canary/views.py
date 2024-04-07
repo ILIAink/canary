efcf15dcd878c468379d3f51d69e1fd2878a6bfd
liongoogle <113564377+liongoogle@users.noreply.github.com>
2024-04-07 18:39:04 -0400

    finalized user dashboard styling

--- src/canary/views.py
+++ src/canary/views.py
@@ -20,10 +20,13 @@ def dashboard(request):
    if request.user.is_authenticated:
        user_communities = CommunityMember.objects.filter(member=request.user, is_admin=False)
        admin_communities = CommunityMember.objects.filter(member=request.user, is_admin=True)
+       both_empty = False
+       if(not(user_communities) and not(admin_communities)):
+            both_empty = True
        if False:#request.user.is_staff:  # Check if the user is an administrator
            return render(request, 'dashboard/dashboard_admin.html', {'user': request.user, 'user_communities': user_communities})
        else:
-           return render(request, 'dashboard/new_user_dashboard.html', {'user': request.user, 'user_communities': user_communities, "admin_communities": admin_communities})
+           return render(request, 'dashboard/dashboard_user_final.html', {'user': request.user, 'user_communities': user_communities, "admin_communities": admin_communities, 'both_empty': both_empty})
    else:
        # Handle the case when the user is not authenticated, perhaps redirect to login page
        return render(request, 'account/login.html')
