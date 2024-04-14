c694432fd99dc42004e389a654e60f784609b365
Nathan Luu <nluu0122@gmail.com>
2024-04-14 09:27:52 -0400

    created notification model

--- src/canary/models.py
+++ src/canary/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from accounts.models import User
+from communities.models import Community, Report
+
 
 # Create your models here.
 
@@ -8,3 +13,18 @@ class UploadedFile(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.file.name
+    
+# notifications model
+class Notification(models.Model):
+    sender = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True, related_name='sender')
+    recipient = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='recipient')
+
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+
+    content = models.TextField()
+
+    is_viewed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
