ac5e8a4514a99c953725958b9895192e4b7581a3
ilia <ysk.iliakhalighi@gmail.com>
2024-07-17 21:07:42 -0400

    Cleaning up

--- unused/deprecated_urls.py
+++
@@ -1,2 +0,0 @@
-# path("join_a_community/", views.join_a_community, name="join_a_community"),
-# path("join_community", views.join_community, name="join_community"),
--- unused/reports/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "reports"
-urlpatterns = [
-    path("create_report/", views.create_report, name="create_report"),
-    path("save_report", views.save_report, name="save_report"),
-]
